21eb1b59f315e366d272e13a33fb68f7e15bed13
Remi Schwartz <schwartz.remi@gmail.com>
2021-02-18 18:34:12 +0100

    thumbnail hover

--- db/seeds.rb
+++ db/seeds.rb
@@ -27,11 +27,12 @@ armand = User.create!(
 #----------------------------------------------------
 puts "Seeding artists..."
 
-paul_taylor = Artist.create!(name: "Paul Taylor")
-issa_doumbia = Artist.create!(name: "Issa Doumbia")
-laura_laune = Artist.create!(name: "Laura Laune")
-elodie_poux = Artist.create!(name: "Elodie Poux")
-jean_racine = Artist.create!(name: "Jean Racine")
+paul_taylor = Artist.create!(name: "Paul AYLOR")
+issa_doumbia = Artist.create!(name: "Issa DOUMBIA")
+laura_laune = Artist.create!(name: "Laura LAUNE")
+elodie_poux = Artist.create!(name: "Elodie POUX")
+ben_artist = Artist.create!(name: "BEN")
+jean_racine = Artist.create!(name: "Jean RACINE")
 
 
 
@@ -50,6 +51,63 @@ so_british.artist = paul_taylor
 so_british.thumbnail.attach(io: File.open('app/assets/images/so_british.png'), filename: 'so_british_photo.png', content_type: 'image/png')
 so_british.wide_poster.attach(io: File.open('app/assets/images/so_british_wide.png'), filename: 'so_british_wide_photo.png', content_type: 'image/png')
 so_british.save!
+
+
+prem_consultation = Show.new(
+  name: "1ere CONSULTATION",
+  description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum eros velit, vulputate at auctor vitae, ultrices vel lacus. Nunc aliquet felis eget ligula molestie, a eleifend nulla rutrum. Suspendisse justo ligula, aliquam at dapibus et, rutrum a odio. Curabitur quis justo ac lectus congue sagittis. Fusce nec porttitor felis. Vestibulum leo diam, dictum vel quam vitae, consectetur rhoncus sapien. Vestibulum vel tellus ullamcorper, rutrum arcu vel, pellentesque urna. Praesent eget maximus enim....",
+  date: "15/11/2020",
+  time: "20:00",
+  price: 8,
+)
+prem_consultation.category = comedy
+prem_consultation.artist = issa_doumbia
+prem_consultation.thumbnail.attach(io: File.open('app/assets/images/prem_consultation.png'), filename: 'prem_consultation_photo.png', content_type: 'image/png')
+# prem_consultation.wide_poster.attach(io: File.open('app/assets/images/prem_consultation_wide.png'), filename: 'prem_consultation_wide_photo.png', content_type: 'image/png')
+prem_consultation.save!
+
+
+diable_gentille_fille = Show.new(
+  name: "Le DIABLE est une gentille fille",
+  description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum eros velit, vulputate at auctor vitae, ultrices vel lacus. Nunc aliquet felis eget ligula molestie, a eleifend nulla rutrum. Suspendisse justo ligula, aliquam at dapibus et, rutrum a odio. Curabitur quis justo ac lectus congue sagittis. Fusce nec porttitor felis. Vestibulum leo diam, dictum vel quam vitae, consectetur rhoncus sapien. Vestibulum vel tellus ullamcorper, rutrum arcu vel, pellentesque urna. Praesent eget maximus enim....",
+  date: "15/11/2020",
+  time: "20:00",
+  price: 8,
+)
+diable_gentille_fille.category = comedy
+diable_gentille_fille.artist = laura_laune
+diable_gentille_fille.thumbnail.attach(io: File.open('app/assets/images/diable_gentille_fille.png'), filename: 'diable_gentille_fille_photo.png', content_type: 'image/png')
+# diable_gentille_fille.wide_poster.attach(io: File.open('app/assets/images/diable_gentille_fille_wide.png'), filename: 'diable_gentille_fille_wide_photo.png', content_type: 'image/png')
+diable_gentille_fille.save!
+
+
+ben = Show.new(
+  name: "BEN",
+  description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum eros velit, vulputate at auctor vitae, ultrices vel lacus. Nunc aliquet felis eget ligula molestie, a eleifend nulla rutrum. Suspendisse justo ligula, aliquam at dapibus et, rutrum a odio. Curabitur quis justo ac lectus congue sagittis. Fusce nec porttitor felis. Vestibulum leo diam, dictum vel quam vitae, consectetur rhoncus sapien. Vestibulum vel tellus ullamcorper, rutrum arcu vel, pellentesque urna. Praesent eget maximus enim....",
+  date: "15/11/2020",
+  time: "20:00",
+  price: 8,
+)
+ben.category = comedy
+ben.artist = ben_artist
+ben.thumbnail.attach(io: File.open('app/assets/images/ben.png'), filename: 'ben_photo.png', content_type: 'image/png')
+# ben.wide_poster.attach(io: File.open('app/assets/images/ben_wide.png'), filename: 'ben_wide_photo.png', content_type: 'image/png')
+ben.save!
+
+
+syndrome_playmobil = Show.new(
+  name: "Le Syndrome du Playmobil",
+  description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum eros velit, vulputate at auctor vitae, ultrices vel lacus. Nunc aliquet felis eget ligula molestie, a eleifend nulla rutrum. Suspendisse justo ligula, aliquam at dapibus et, rutrum a odio. Curabitur quis justo ac lectus congue sagittis. Fusce nec porttitor felis. Vestibulum leo diam, dictum vel quam vitae, consectetur rhoncus sapien. Vestibulum vel tellus ullamcorper, rutrum arcu vel, pellentesque urna. Praesent eget maximus enim....",
+  date: "15/11/2020",
+  time: "20:00",
+  price: 8,
+)
+syndrome_playmobil.category = comedy
+syndrome_playmobil.artist = elodie_poux
+syndrome_playmobil.thumbnail.attach(io: File.open('app/assets/images/syndrome_playmobil.png'), filename: 'syndrome_playmobil_photo.png', content_type: 'image/png')
+# syndrome_playmobil.wide_poster.attach(io: File.open('app/assets/images/syndrome_playmobil_wide.png'), filename: 'syndrome_playmobil_wide_photo.png', content_type: 'image/png')
+syndrome_playmobil.save!
+
 
 andromaque = Show.new(
   name: "Andromaque",
