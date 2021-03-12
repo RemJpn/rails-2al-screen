359695fc835cf20caa0baaf6edabcf35e97d064c
Remi Schwartz <schwartz.remi@gmail.com>
2021-03-12 10:49:06 +0100

    added booking controller

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   resources :shows, only: [:show]
+  resources :bookings, only: [:new]
 
 end
