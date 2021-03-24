afad12c4576f0406d4a57d937bc83b0f223bfdb3
Remi Schwartz <schwartz.remi@gmail.com>
2021-03-24 10:35:27 +0100

    sliding menu hover, close button to right side

--- app/javascript/components/slidingmenu.js
+++ app/javascript/components/slidingmenu.js
@@ -1,7 +1,34 @@
 const toggleMenu = () => {
   const slidingMenu = document.querySelector('.sliding-menu');
+  const menuOpen = document.querySelector('.sliding-menu-open');
+  menuOpen.removeEventListener('mouseenter', hoverMenu);
+
   slidingMenu.classList.toggle('sliding-menu-active');
+  slidingMenu.classList.remove('sliding-menu-hover');
+
+  slidingMenu.addEventListener('transitionend', () => {
+    console.log('transitionend');
+    const menuOpen = document.querySelector('.sliding-menu-open');
+    menuOpen.addEventListener('mouseenter', hoverMenu);
+  });
+
+
 }
+
+const hoverMenu = () => {
+  const slidingMenu = document.querySelector('.sliding-menu');
+  if (!slidingMenu.classList.contains('sliding-menu-active')) {
+    slidingMenu.classList.add('sliding-menu-hover');
+  }
+}
+const unhoverMenu = () => {
+  const slidingMenu = document.querySelector('.sliding-menu');
+  // if (!slidingMenu.classList.includes('sliding-menu-active')) {
+    slidingMenu.classList.remove('sliding-menu-hover');
+  // }
+}
+
+
 
 const initSlidingMenu = () => {
   if (document.querySelector('.sliding-menu')) {
@@ -10,6 +37,9 @@ const initSlidingMenu = () => {
 
     menuOpen.addEventListener('click', toggleMenu);
     menuClose.addEventListener('click', toggleMenu);
+
+    menuOpen.addEventListener('mouseenter', hoverMenu);
+    menuOpen.addEventListener('mouseleave', unhoverMenu );
   }
 }
 
