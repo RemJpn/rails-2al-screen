acd817e38aeb5ab21d68bf171f4afaab67a5aa0c
Remi Schwartz <schwartz.remi@gmail.com>
2021-03-25 11:47:31 +0100

    permit new user parameters

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,20 @@
 class ApplicationController < ActionController::Base
   before_action :store_user_location!, if: :storable_location?
   before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   def after_sign_in_path_for(resource)
     stored_location_for(resource) || super
   end
+
+  def configure_permitted_parameters
+    # For additional fields in app/views/devise/registrations/new.html.erb
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :city, :postcode, :address, :phone, :country])
+
+    # For additional in app/views/devise/registrations/edit.html.erb
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :city, :postcode, :address, :phone, :country])
+  end
+
 
   private
     def storable_location?
